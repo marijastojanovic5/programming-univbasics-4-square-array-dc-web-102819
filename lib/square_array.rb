e1293306d4418cf1af014734a134c86d4fc244c9
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-09-24 18:13:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
-  # your code here
+def squar(array)
+  counter=0
+  
+  while  counter<array.length do
+    puts array[counter]
+     square_array(array)
+    counter +=1
+ 
+ 
+end
+
 end
