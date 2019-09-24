a982b76918e18060f24ccabb120956f0fa06e4e4
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-09-24 18:44:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,10 +3,10 @@ def square_array(array)
   arr=[]
   
   while  counter<array.length do
-    puts array[counter]**2
-    
-   counter +=1
     puts arr<< array[counter]**2
+    counter +=1
+    p arr
+   
 end
 
 
